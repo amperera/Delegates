604789f07b25bac991d3eab4c2bf33dc7b484900
Amila <madusanka.lk@gmail.com>
2014-08-03 20:32:45 +0530

    new

---
+++ Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    delegate void mydelegate(int number);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mydelegate call = SayHellow;
+
+            executeTheDelegate(2 , call);
+            Console.ReadLine();
+            
+        }
+
+        public static void SayHellow(int number)
+        {
+            Console.WriteLine("{0} " ,number);
+        }
+
+        static void executeTheDelegate(int num , mydelegate calldelegate)
+        {
+            calldelegate(num);
+        }
+    }
+}
